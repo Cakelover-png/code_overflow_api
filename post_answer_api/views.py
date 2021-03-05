0d70dfbbfc71d468e9d75f7a5a08e7c6ee4af1eb
Cakelover-png <NamelessAlien1@gmail.com>
2021-03-05 23:46:42 +0400

    minor fixes in UserPostDetailView

--- post_answer_api/views.py
+++ post_answer_api/views.py
@@ -1,5 +1,5 @@
 from rest_framework import status
-from rest_framework.authentication import TokenAuthentication
+# from rest_framework.authentication import TokenAuthentication
 from rest_framework.filters import SearchFilter
 from rest_framework.generics import ListCreateAPIView, RetrieveUpdateDestroyAPIView
 from rest_framework.permissions import IsAuthenticatedOrReadOnly, IsAuthenticated
@@ -15,8 +15,8 @@ class UserPostView(ListCreateAPIView):
     queryset = UserPost.objects.all()
     # authentication_classes = (TokenAuthentication, )
     permission_classes = (IsAuthenticatedOrReadOnly,)
-    filter_backends = (SearchFilter, )
-    search_fields = ('title', )
+    filter_backends = (SearchFilter,)
+    search_fields = ('title',)
 
     def perform_create(self, serializer):
         serializer.save(user=self.request.user)
@@ -28,9 +28,6 @@ class UserPostDetailView(RetrieveUpdateDestroyAPIView):
     # authentication_classes = (TokenAuthentication, )
     permission_classes = (IsAuthenticated, IsOwnerOrReadOnly)
 
-    def perform_create(self, serializer):
-        serializer.save(user=self.request.user)
-
 
 class UserAnswerView(ListCreateAPIView):
     serializer_class = UserAnswerSerializer
@@ -39,7 +36,7 @@ class UserAnswerView(ListCreateAPIView):
     permission_classes = (IsAuthenticated,)
 
     def post(self, request, *args, **kwargs):
-        if self.queryset.filter(user=request.user, user_post=request.data['user_post']):
+        if self.queryset.filter(user=request.user, user_post=request.data['user_post']).exists():
             return Response({'Details': 'You already answered to this post'}, status=status.HTTP_400_BAD_REQUEST)
         else:
             return self.create(request, *args, **kwargs)
